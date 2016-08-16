f8beed793adf248da1d373ebbf82040527e73dec
Ya-Te Wong <yate.wong@fuhu.com>
2016-08-15 17:57:04 -0700

    Jenkins: releasing company-service-ci-test-module 1.0.1

--- build_numbers.sh
+++ build_numbers.sh
@@ -5,11 +5,11 @@ MAJOR_BRANCH=1.0
 
 # 2) bump up this RELEASE_NUMBER for every minor release (within a major branch), 
 #    as the last number of a complete release number, such as the '1' in '5.0.1'
-RELEASE_NUMBER=0
+RELEASE_NUMBER=1
 
 # no need to modify the followings starting from here:
 REMOTE=origin
 TRUNK=master
 PROJECT=company-service-ci-test-module
 # to force making changes to this file for differentiating builds in Jenkins
-JENKINS_BUILD_NUMBER=0
+JENKINS_BUILD_NUMBER=18
