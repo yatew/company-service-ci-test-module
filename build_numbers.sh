b74871c4d0f388e4d1a69ab0e67df7a71b300c74
Ya-Te Wong <yate.wong@fuhu.com>
2016-08-17 10:48:13 -0700

    Jenkins: releasing company-service-ci-test-module 4.0.0

--- build_numbers.sh
+++ build_numbers.sh
@@ -1,7 +1,7 @@
 # definition of release/build numbers for manual and Jenkins semi-auto branching/releasing.
 
 # 1) only need to increment this MAJOR_BRANCH for major releases, such as 1.0, 1.1, 2.0, etc.
-MAJOR_BRANCH=1.0
+MAJOR_BRANCH=4.0
 
 # 2) bump up this RELEASE_NUMBER for every minor release (within a major branch), 
 #    as the last number of a complete release number, such as the '1' in '5.0.1'
@@ -12,4 +12,4 @@ REMOTE=origin
 TRUNK=master
 PROJECT=company-service-ci-test-module
 # to force making changes to this file for differentiating builds in Jenkins
-JENKINS_BUILD_NUMBER=0
+JENKINS_BUILD_NUMBER=21
